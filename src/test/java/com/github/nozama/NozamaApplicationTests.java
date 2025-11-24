70abd08c41a26917e1eab37ec33262794c90788a
joseeneto19 <josemnetodev@gmail.com>
2025-11-24 17:23:04 -0300

    first commit

---
+++ src/test/java/com/github/nozama/NozamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nozama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NozamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
