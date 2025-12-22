4e65f95714c1b723a6f33ecc8983d64cbe3fe807
joseeneto19 <josemnetodev@gmail.com>
2025-12-22 12:49:09 -0300

    feat: adiciona DTO e o metodo findById

---
+++ src/main/java/com/github/nozama/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.github.nozama.controllers;
+
+import com.github.nozama.dto.ProductDTO;
+import com.github.nozama.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+}
